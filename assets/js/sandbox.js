2141e7534c7443c1558758fc3809ccb3886a882b
julsBasco <84578324+julsBasco@users.noreply.github.com>
2021-08-08 11:00:11 +0800

    main is good

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,32 +1,81 @@
 let card = document.querySelector(".card");
-let img_card = document.querySelector(".card_img");
+let imgJuls = document.querySelector('.juls-display');
 let note_card = document.querySelector(".card_note");
 let quoteGrid_card = document.querySelector(".card_quoteGrid");
 let mediaMedium = window.matchMedia("(min-width: 769px)")
 
+// event handlers
+const changeDisplayJuls = () => {
 
-// const changeDisplay = () => {
-    
-//     if(mediaMedium.matches){
-//         switch(img_card.style.display){
-//             case "": 
-//                 img_card.style.display = "none";
-//                 note_card.style.display ="grid";
-//                 quoteGrid_card.style.display = "grid";
-//                 break;
+    if(mediaMedium.matches){
+        document.getElementById('img-juls').style.display = "none";
 
-//             case "none":
-//                 img_card.style.display = "";
-//                 note_card.style.display ="none";
-//                 quoteGrid_card.style.display = "none";
-//                 break; 
+    }
+}
+
+const changeDisplayDan = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-dan').style.display = "none";
+
+    }
+}
+
+const changeDisplayLem = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-lem').style.display = "none";
+
+    }
+}
+
+const changeDisplayIvan = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-ivan').style.display = "none";
+
+    }
+}
+
+const changeDisplayMicah = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-micah').style.display = "none";
+
+    }
+}
+
+const changeDisplayNads = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-nads').style.display = "none";
+
+    }
+}
+
+const changeDisplayBianca = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-bianca').style.display = "none";
+
+    }
+}
+
+const changeDisplayCn = () => {
+
+    if(mediaMedium.matches){
+        document.getElementById('img-cn').style.display = "none";
+
+    }
+}
 
 
-//         }
-        
-//     }
-// }
-
-
-document.querySelector('.juls').addEventListener('click', changeDisplay);
-document.querySelector('.dan').addEventListener('click', changeDisplay);
+// event listeners
+document.querySelector('.juls').addEventListener('click', changeDisplayJuls);
+document.querySelector('.dan').addEventListener('click', changeDisplayDan);
+document.querySelector('.lem').addEventListener('click', changeDisplayLem);
+document.querySelector('.ivan').addEventListener('click', changeDisplayIvan);
+document.querySelector('.micah').addEventListener('click', changeDisplayMicah);
+document.querySelector('.nads').addEventListener('click', changeDisplayNads);
+document.querySelector('.bianca').addEventListener('click', changeDisplayBianca);
+document.querySelector('.cn').addEventListener('click', changeDisplayCn);
